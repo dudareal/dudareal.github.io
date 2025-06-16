b21a41f4e613b9a923b80d6e412d055a2c7e0d57
dudareal <dudarealferreira07@gmail.com>
2025-06-16 15:40:19 -0300

    oirs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
 const Header = () => {
-  const profilePhotoUrl = 'https://www.instagram.com/p/DKx48nZyH4J_zRaeVPASRDmkwmXiVM3HZSecfU0/?igsh=MTRzczQ0NjJhbTcxdw==';
   return (
     <header className="header">
       <div className="profile-section">
         <div className="profile-photo">
-          <img src={profilePhotoUrl} alt="" />
         </div>
         <h1 className="name">Eduarda Real</h1>
         <div className="age-email">
